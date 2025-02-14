8b84ee709f2b7f8119b6f97fcc02cd188e032b94
AnhNguyen <anhthvt6241@gmai.com>
2025-02-14 14:50:47 +0700

    Delete file not used

--- DeepWise.h
+++
@@ -1,6 +0,0 @@
-#ifndef DEEPWISE
-#define DEEPWISE
-float *** DeepWise(float *** ifmap, float ****kernel, int input_height, int input_width, int input_channel, int kernel_height,  int kernel_filter,int kernel_channel, int stride, int padding);
-
-
-#endif
--- Padding.h
+++
@@ -1,6 +0,0 @@
-#ifndef PADDING
-#define PADDING
-float ***Padding_3D(float ***ifmap, int input_height, int input_width, int input_channel, int padding);
-float **Padding_2D(float **ifmap, int input_height, int input_width, int padding);
-
-#endif
