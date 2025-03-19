9af47f06515d24115aaaebe172c996f7a6fc01df
“RiMuRuTemPrst” <tquocanh2309@gmail.com>
2025-03-19 11:40:23 +0700

    Fix BRAM

--- dut/BRAM.v
+++ dut/BRAM.v
@@ -3,13 +3,13 @@ module BRAM(
     input wire we,                     // Write enable
     input wire [6:0] wr_addr,           // Write address (6-bit → 64 hàng)
     input wire [19:0] rd_addr,           // Read address (6-bit → 64 hàng)
-    input wire [63:0] data_in,          // Dữ liệu đầu vào 64-bit
+    input wire [31:0] data_in,          // Dữ liệu đầu vào 64-bit
     output reg [31:0] data_out      // Đầu ra 2048-bit (256*8 bit)
     //output reg [19:0] addr
 );
 
     // Dùng 32 khối BRAM chạy song song, mỗi khối lưu 64-bit
-    (* ram_style = "block" *) reg [31:0] bram [0:65536];  // Dùng 1 mảng một chiều
+    (* ram_style = "block" *) reg [31:0] bram [0:65535];  // Dùng 1 mảng một chiều
     
     //integer i;
     always @(posedge clk) begin
